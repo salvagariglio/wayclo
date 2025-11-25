32d7c3f2c3e64741bbabe826899df21807a29945
salvagariglio <salvaglinkedin@gmail.com>
2025-11-25 12:06:29 -0300

    ..

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -2,16 +2,16 @@ import nodemailer from "nodemailer";
 
 export function getTransporter() {
     return nodemailer.createTransport({
-        host: process.env.SMTP_HOST,        // smtp-mail.outlook.com
-        port: Number(process.env.SMTP_PORT), // 587
-        secure: false,                       // 🔥 correcto para Outlook
+        host: process.env.SMTP_HOST,           // smtp.office365.com
+        port: Number(process.env.SMTP_PORT),   // 587
+        secure: false,                         // TLS via STARTTLS
+        requireTLS: true,                      // 🔥 OBLIGATORIO PARA MICROSOFT 365
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
         },
         tls: {
-            ciphers: "SSLv3"
-        }
+            ciphers: "TLSv1.2", // 🔥 Office365 exige TLS moderno
+        },
     });
 }
-
